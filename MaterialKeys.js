23a6c6d3f7a8fb76134aa4aca12eaf28bf35a8a0
CDeenen <cdeenen@outlook.com>
2020-11-18 02:42:33 +0100

    v1.1.0

---
+++ MaterialKeys.js
@@ -0,0 +1,243 @@
+import {registerSettings} from "./src/settings.js";
+import {Launchpad} from "./src/Launchpad.js";
+import {PlaylistControl} from "./src/playlist.js";
+import {SoundboardControl} from "./src/soundboard.js";
+import {VisualFx} from "./src/visualFx.js";
+import {CombatTracker} from "./src/combatTracker.js";
+import {MacroBoard} from "./src/macroBoard.js";
+export const moduleName = "MaterialKeys";
+export const launchpad = new Launchpad();
+export const playlistControl = new PlaylistControl();
+export const soundboard = new SoundboardControl();
+export const visualFx = new VisualFx();
+export const combatTracker = new CombatTracker();
+export const macroBoard = new MacroBoard();
+
+//Websocket variables
+var ws;                         //Websocket variable
+let wsOpen = false;             //Bool for checking if websocket has ever been opened => changes the warning message if there's no connection
+let wsInterval;                 //Interval timer to detect disconnections
+let WSconnected = false;
+
+//Other global variables
+let enableModule;
+let activeSounds = [];
+        
+/**
+ * Analyzes the message received from the IR tracker.
+ * If coordinates are received, scale the coordinates to the in-game coordinate system, find the token closest to those coordinates, and either take control of a new token or update the position of the image of that token
+ * If no coordinates are received, move token to last recieved position
+ * 
+ * @param {*} msg Message received from the IR tracker
+ */
+async function analyzeWSmessage(msg){
+    //console.log(msg);
+    const data = JSON.parse(msg);
+    //console.log(data);
+    if (data.target != 'MK') return;
+    
+    if (data.type == 'key'){
+        launchpad.keypress(data);
+    }
+    else if (data.type == 'connected'){
+        const connectedDevice = data.data;
+        if (connectedDevice == 'null') ui.notifications.warn("Material Keys: "+game.i18n.localize("MaterialKeys.Notifications.NoMidi"));
+        else {
+            ui.notifications.notify("Material Keys: "+game.i18n.localize("MaterialKeys.Notifications.MidiConnect")+" "+connectedDevice);
+            launchpad.setBrightness(game.settings.get(moduleName,'brightness'));
+            launchpad.setMode(launchpad.keyMode);
+        }
+    }
+};
+
+export function playTrack(soundNr,play,repeat,volume){
+    if (play){
+        const trackId = game.settings.get(moduleName,'soundboardSettings').sounds[soundNr];
+        const playlistId = game.settings.get(moduleName,'soundboardSettings').playlist;
+        const sounds = game.playlists.entities.find(p => p._id == playlistId).data.sounds;
+        const sound = sounds.find(p => p._id == trackId);
+        if (sound == undefined){
+            activeSounds[soundNr] = false;
+            return;
+        }
+        volume *= game.settings.get("core", "globalInterfaceVolume");
+        const src = sound.path;
+
+        let howl = new Howl({src, volume, loop: repeat, onend: (id)=>{
+            if (repeat == false){
+                activeSounds[soundNr] = false;
+            }
+        },
+        onstop: (id)=>{
+            activeSounds[soundNr] = false;
+        }});
+        howl.play();
+        activeSounds[soundNr] = howl;
+   }
+   else {
+       activeSounds[soundNr].stop();
+   }
+}
+
+Hooks.on('renderPlaylistDirectory', (playlistDirectory)=>{
+    if (enableModule == false) return;
+    if (WSconnected) {
+        playlistControl.playlistUpdate();
+        playlistControl.volumeUpdate();
+    }
+});
+
+Hooks.on('updateScene',()=>{
+    if (enableModule == false) return;
+    if (WSconnected) visualFx.update();
+})
+
+Hooks.on('renderCombatTracker', (a,b,combat)=>{
+    if (enableModule == false) return;
+    if (WSconnected){
+        combatTracker.trackerUpdate(combat.combat);
+        if (combat.hasCombat)
+            combatTracker.updateTokens(combat.combat);
+        else
+            combatTracker.updateTokens(null);
+            combatTracker.hpUpdate(combat.combat);
+    }
+});
+
+Hooks.once('init', ()=>{
+    //CONFIG.debug.hooks = true;
+    registerSettings(); //in ./src/settings.js
+})
+
+Hooks.once('ready', ()=>{
+    enableModule = (game.settings.get(moduleName,'Enable') && game.user.isGM) ? true : false;
+    if (enableModule) 
+        startWebsocket();
+    
+    for (let i=0; i<64; i++)
+            activeSounds[i] = false;
+        game.socket.on(`module.MaterialKeys`, (payload) =>{
+            //console.log(payload);
+            if (payload.msgType != "playSound") return;
+            playTrack(payload.trackNr,payload.play,payload.repeat,payload.volume);
+    });
+    if (game.user.isGM == false) return;
+    const soundBoardSettings = game.settings.get(moduleName,'soundboardSettings');
+    const macroSettings = game.settings.get(moduleName, 'macroSettings');
+    let array = [];
+    for (let i=0; i<64; i++) array[i] = "";
+    let arrayVolume = [];
+    for (let i=0; i<64; i++) arrayVolume[i] = "50";
+    let arrayZero = [];
+    for (let i=0; i<64; i++) arrayZero[i] = "0";
+
+    if (macroSettings.color == undefined){
+        game.settings.set(moduleName,'macroSettings',{
+            macros: array,
+            color: arrayZero
+        });
+    }
+    if (soundBoardSettings.colorOff == undefined){
+        game.settings.set(moduleName,'soundboardSettings',{
+            playlist: "",
+            sounds: array,
+            colorOn: arrayZero,
+            colorOff: arrayZero,
+            mode: arrayZero,
+            toggle: arrayZero,
+            volume: arrayVolume
+        });
+    }
+});
+
+Hooks.on('closeApplication', (form)=>{
+    if (enableModule == false) return;
+    if (form.id == "playlist-config" && WSconnected){
+        launchpad.setMainLEDs(0,0);
+        playlistControl.playlistUpdate();
+        playlistControl.volumeUpdate();
+    }
+});
+
+/**
+ * Start a new websocket
+ * Start a 10s interval, if no connection is made, run resetWS()
+ * If connection is made, set interval to 1.5s to check for disconnects
+ * If message is received, reset the interval, and send the message to analyzeWSmessage()
+ */
+function startWebsocket() {
+    //ip = game.settings.get(moduleName,'IP');
+    const address = game.settings.get(moduleName,'address');
+    ws = new WebSocket('ws://'+address+'/');
+
+    ws.onmessage = function(msg){
+       // console.log(msg);
+        analyzeWSmessage(msg.data);
+        clearInterval(wsInterval);
+        wsInterval = setInterval(resetWS, 5000);
+    }
+
+    ws.onopen = function() {
+        WSconnected = true;
+        const msg = {
+            target: "server",
+            module: "MK"
+        }
+        sendWS(JSON.stringify(msg));
+        launchpad.setMode(8);
+        const address = game.settings.get(moduleName,'address');
+        ui.notifications.info("Material Keys: "+game.i18n.localize("MaterialKeys.Notifications.WSConnect")+" "+address);
+        wsOpen = true;
+        clearInterval(wsInterval);
+        wsInterval = setInterval(resetWS, 5000);
+    }
+  
+    clearInterval(wsInterval);
+    wsInterval = setInterval(resetWS, 10000);
+}
+
+/**
+ * Try to reset the websocket if a connection is lost
+ */
+function resetWS(){
+    if (wsOpen) ui.notifications.warn("Material Keys: "+game.i18n.localize("MaterialKeys.Notifications.WSDisconnect"));
+    else ui.notifications.warn("Material Keys: "+game.i18n.localize("MaterialKeys.Notifications.WSCantConnect"));
+    WSconnected = false;
+    startWebsocket();
+}
+
+export function sendWS(txt){
+    if (WSconnected) ws.send(txt);
+}
+
+function checkCombat(){
+    if (game.combat) 
+        return game.combat.started;
+    else return false; 
+}
+
+export function getFromJSONArray(data,i){
+    if (i>8) return 'nul';
+    let val;
+    if (i == 0) val = data.a;
+    else if (i == 1) val = data.a;
+    else if (i == 2) val = data.c;
+    else if (i == 3) val = data.d;
+    else if (i == 4) val = data.e;
+    else if (i == 5) val = data.f;
+    else if (i == 6) val = data.g;
+    else if (i == 7) val = data.h;
+    return val;
+}
+
+export function setToJSONArray(data,i,val){
+    if (i>8) return 'nul';
+    if (i == 0) data.a = val;
+    else if (i == 1) data.b = val;
+    else if (i == 2) data.c = val;
+    else if (i == 3) data.d = val;
+    else if (i == 4) data.e = val;
+    else if (i == 5) data.f = val;
+    else if (i == 6) data.g = val;
+    else if (i == 7) data.h = val;
+}
---
+++ src/combatTracker.js
@@ -0,0 +1,232 @@
+import * as MODULE from "../MaterialKeys.js";
+import {launchpad} from "../MaterialKeys.js";
+
+export class CombatTracker{
+    constructor(){
+        this.combatantsLengthOld = 0;
+        this.combatState = 0;
+    }
+
+    updateTokens(combat){
+        if (launchpad.keyMode != 4) return;
+        if (combat != null){
+            let combatants = combat.combatants;
+            if (combatants.length > 0){
+                let initiativeOrder = combat.turns;
+                let oldCombatants = 0;
+                if (this.combatantsLengthOld > combatants.length){
+                    oldCombatants = this.combatantsLengthOld-combatants.length;
+                    for (let i=0; i<this.combatantsLengthOld; i++){
+                        let j = i + initiativeOrder.length;
+                        if (i>7) j = i-18;
+                        if (i>15) j = i-36;
+                        if (i>23) j = i-54;
+                        if (i>31) break;
+                        let led = 81+j;
+                        launchpad.setLED(led,0,0);
+                    }     
+                }
+                for (let i=0; i<initiativeOrder.length; i++){
+                    let token = initiativeOrder[i].token;
+                    //console.log("token: ",token);
+                    //console.log("initiativeOrder: ",initiativeOrder);
+                    let color;
+                    if (token.disposition == 1) color = 87;
+                    else if (token.disposition == 0) color = 74;
+                    else if (token.disposition == -1) color = 72;
+                    let type = 0;
+                    if (combat.started && token._id == combat.combatant.tokenId) type =2;
+                    let j = i;
+                    if (i>7) j = i-18;
+                    if (i>15) j = i-36;
+                    if (i>23) j = i-54;
+                    if (i>31) break;
+                    let led = 81+j;
+                    
+                    if (initiativeOrder[i].defeated)
+                        launchpad.setLED(led,type,3);
+                    else
+                        launchpad.setLED(led,type,color);
+                }
+                
+                this.combatantsLengthOld = combatants.length;
+                launchpad.updateLEDs();
+                return;
+            }
+        }
+        if (this.combatantsLengthOld > 0){
+            for (let i=0; i<this.combatantsLengthOld; i++){
+                let j = i;
+                if (i>7) j = i-18;
+                if (j>15) j = i-28;
+                let led = 81+j;
+                launchpad.setLED(led,0,0);
+            } 
+            launchpad.updateLEDs();    
+        }
+    }
+
+    trackerKeyPress(key){
+        if(key == 14 || key == 15 || key == 24 || key == 25 || key == 34 || key == 35){
+            if (game.combat.started == false) game.combat.startCombat();
+            else game.combat.endCombat();
+        }
+        else if (key == 11 || key == 12 || key == 21 || key == 22 || key == 31 || key == 32){
+            game.combat.previousTurn();
+        }
+        else if (key == 17 || key == 18 || key == 27 || key == 28 || key == 37 || key == 38){
+            game.combat.nextTurn();
+        }
+        else {
+            let selected = key - 81;
+            if (selected < 0) selected += 18;
+            if (selected < 0) selected += 18;
+            
+            let token = game.combat.turns[selected].token;
+            if (token != undefined){
+                let tokenId = token._id;
+                let tokens = canvas.tokens.children[0].children;
+                for (let i=0; i<tokens.length; i++){
+                    if (tokens[i].id == tokenId){
+                        tokens[i].name;
+                        tokens[i].control();
+                    }    
+                }
+                canvas.animatePan({x: token.x, y: token.y, speed: 2000});
+            }
+        }
+    }
+
+    trackerUpdate(combat){
+        if (launchpad.keyMode != 4) return;
+        if (combat != undefined){
+            let combatants = combat.combatants;
+            if (combatants.length > 0){
+                if (combat.started)
+                    this.combatState = 2; 
+                else 
+                    this.combatState = 1;
+                this.updateTokens(combat);    
+            }
+            else 
+                this.combatState = 0;
+        }
+        else 
+            this.combatState = 0;
+
+        let mode = 2;
+        let color = 87;
+        let modeArrows = 2;
+        let colorArrows = 72;
+        if (this.combatState == 0){
+            launchpad.setMainLEDs(0,0);
+        }
+        if (this.combatState == 1) {
+            mode = 0;
+        }
+        else if (this.combatState == 2){
+            mode = 0;
+            color = 72;
+            modeArrows = 0;
+            colorArrows = 87;
+        }
+        launchpad.setLED(14,mode,color);
+        launchpad.setLED(15,mode,color);
+        launchpad.setLED(24,mode,color);
+        launchpad.setLED(25,mode,color);
+        launchpad.setLED(34,mode,color);
+        launchpad.setLED(35,mode,color);
+        launchpad.setArrow(17,'right',colorArrows,modeArrows);
+        launchpad.setArrow(11,'left',colorArrows,modeArrows);
+
+        launchpad.updateLEDs();
+    }
+
+    hpKeyPress(key){
+        if (Math.floor(launchpad.keyMode/10) != 3) return;
+        let page = launchpad.keyMode % 10-1;
+        let selected = key % 10 - 1 + 8 * page;
+        let token = game.combat.turns[selected].token;
+        if (token != undefined){
+            let tokenId = token._id;
+            let tokens = canvas.tokens.children[0].children;
+            for (let i=0; i<tokens.length; i++){
+                if (tokens[i].id == tokenId){
+                    tokens[i].name;
+                    tokens[i].control();
+                }    
+            }
+            canvas.animatePan({x: token.x, y: token.y, speed: 2000});
+        }
+    }
+
+    hpUpdate(combat){
+        if (Math.floor(launchpad.keyMode/10) != 3) return;
+        let hpTrackerPages;
+        if (game.combat) hpTrackerPages = Math.ceil(game.combat.combatants.length/8);
+        let color;
+        let type;
+        type = (launchpad.keyMode == 31)? 1 : 2;
+        color = (hpTrackerPages>1)? 87 : 0;
+        launchpad.setLED(79,type,color);
+        type = (launchpad.keyMode == 32)? 1 : 2;
+        color = (hpTrackerPages>1)? 79 : 0;
+        launchpad.setLED(69,type,color);
+        type = (launchpad.keyMode == 33)? 1 : 2;
+        color = (hpTrackerPages>2)? 53 : 0;
+        launchpad.setLED(59,type,color);
+        type = (launchpad.keyMode == 34)? 1 : 2;
+        color = (hpTrackerPages>3)? 74 : 0;
+        launchpad.setLED(49,type,color);
+
+        let page = launchpad.keyMode % 10-1;
+        let combatants = combat.combatants;
+        
+        launchpad.setMainLEDs(0,0);
+        if (combatants.length > 0){
+            let initiativeOrder = combat.turns;
+        
+            for (let i=0; i<8; i++){
+                let nr = i+8*page;
+                if (nr >= initiativeOrder.length) break;
+                let token = initiativeOrder[nr].token;
+                let color;
+
+                if (token.disposition == 1) color = 87;
+                else if (token.disposition == 0) color = 74;
+                else if (token.disposition == -1) color = 72;
+                
+                let type = 0;
+                if (combat.started && token._id == combat.combatant.tokenId) type = 2;
+                let j = i;
+                if (i>7) j = i-18;
+                if (j>15) j = i-28;
+                
+            
+                let led = 91+i;
+
+                if (initiativeOrder[nr].defeated)
+                    launchpad.setLED(led,type,3);
+                else
+                    launchpad.setLED(led,type,color);
+                
+                token = canvas.tokens.children[0].children.find(p => p.id == token._id);
+                let leds = 0;
+
+                let hp = token.actor.data.data.attributes.hp.value;
+                let hpMax = token.actor.data.data.attributes.hp.max;
+                if (hp == 0 || initiativeOrder[nr].defeated) leds = 0;
+                else leds = Math.ceil(8*hp/hpMax);
+
+                for (let j=0; j<8; j++){
+                    let led = 11+10*j+i;
+                    if (j>=leds) 
+                        color = 0; 
+                    launchpad.setLED(led,type,color);
+                }
+            }
+            this.combatantsLengthOld = combatants.length;
+        }
+        launchpad.updateLEDs();
+    }
+}
